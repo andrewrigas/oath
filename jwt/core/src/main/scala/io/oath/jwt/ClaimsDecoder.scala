bd4458090708a8016749a7833985e9c6a7eee6f0
Andreas Rigas <andreas.rigas@outlook.com>
2022-11-14 20:50:09 +0000

    runLint

--- jwt/core/src/main/scala/io/oath/jwt/ClaimsDecoder.scala
+++ jwt/core/src/main/scala/io/oath/jwt/ClaimsDecoder.scala
@@ -2,7 +2,6 @@ package io.oath.jwt
 
 import io.oath.jwt.model.JwtVerifyError
 
-
 trait ClaimsDecoder[T] {
   def decode(token: String): Either[JwtVerifyError.DecodingError, T]
 }
--- jwt/core/src/test/scala/io/oath/jwt/config/JwtManagerLoaderSpec.scala
+++ jwt/core/src/test/scala/io/oath/jwt/config/JwtManagerLoaderSpec.scala
@@ -2,7 +2,6 @@ package io.oath.jwt.config
 
 import io.oath.jwt.testkit.AnyWordSpecBase
 
-
 class JwtManagerLoaderSpec extends AnyWordSpecBase {
 
   "asd" should {
